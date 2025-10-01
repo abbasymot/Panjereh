78d1a5ed8cde42fbb92f4f9cc9ad348a160bdeb4
Amirabbas Motamedi <motameditalemi@gmail.com>
2025-10-01 20:55:29 +0330

    Add project files.

---
+++ Panjereh/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Panjereh
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
